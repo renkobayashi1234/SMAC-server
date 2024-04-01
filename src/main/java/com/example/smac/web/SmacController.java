767b90730ed9f4a7c41d11dd237885f3cc82897f
renkobayashi1234 <ren_kobayashi@jbat.co.jp>
2024-04-01 15:18:07 +0900

    separate socket function

--- src/main/java/com/example/smac/web/SmacController.java
+++ src/main/java/com/example/smac/web/SmacController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -70,7 +71,7 @@ public class SmacController {
     }
 
     @CrossOrigin
-    @PostMapping("/put")
+    @PutMapping("/put")
     public String put( @RequestParam(value = "alertNo", defaultValue = "0") String alertNo) {
         //TODO: process POST request
         logger.info("put");
